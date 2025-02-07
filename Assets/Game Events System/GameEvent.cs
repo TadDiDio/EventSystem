c60ceea7b3ba4fbf85a34cb9f6814564f914646e
TadDiDio <taddidio@vt.edu>
2025-02-07 15:49:22 -0500

    Cleaned up and removed examples

--- Assets/Game Events System/GameEvent.cs
+++ Assets/Game Events System/GameEvent.cs
@@ -1,116 +1,121 @@
 using System;
 using UnityEngine.Events;
 
-public interface GameEventBase 
+namespace EventSystem
 {
-    public void Add(Delegate action);
-    public void Remove(Delegate action);
-    public void Invoke();
-}
-/* 
- * Wrapper class for a default UnityAction event to
- * provide a unified interface for the generic version
- */
-public class GameEvent : GameEventBase
-{
-    private UnityAction Event;
-    public void Add(UnityAction action)
+    public interface GameEventBase
     {
-        Event += action;
+        public void Add(Delegate action);
+        public void Remove(Delegate action);
+        public void Invoke();
     }
-    public void Add(Delegate action)
+    /* 
+     * Wrapper class for a default UnityAction event to
+     * provide a unified interface for the generic version
+     */
+    public class GameEvent : GameEventBase
     {
-        if (action is UnityAction unityAction)
-        Event += unityAction;
-    }
-    public void Remove(UnityAction action)
-    {
-        Event -= action;
-    }
-    public void Remove(Delegate action)
-    {
-        if (action is UnityAction unityAction)
-        { 
-            Event -= unityAction; 
-        }
-    }
-
-    public void Invoke()
-    {
-        Event?.Invoke();
-    }
-}
-
-/*
- * We purposefully only provide an interface with a single 
- * generic type to encourage clients to define custom data
- * types whose intent is more clear than that of their 
- * primative counterparts.
- * 
- * Example: Which is immediately more clear?
- * 
- * 1. CuddleStarted?.Invoke(true, 8, "Tomorrow"); 
- * 
- *         -- or --
- * 
- * 2. struct CuddleData
- *    {
- *       public bool holdHands;
- *       public float duration;
- *       public string subtitle;
- *    }
- * 
- *    CuddleStarted?.Invoke(cuddleData);
- *    
- * Clearly, option 2 gives much more detail as to the 
- * purpose of the parameters. This is exactly the same for 
- * all number of parameters INCLUDING 1.
- */
-public class GameEvent<T> : GameEventBase
-{
-    private UnityAction<T> Event;
-    public T Latest;
-
-    public void Add(UnityAction<T> action)
-    {
-        Event += action;
-    }
-    public void Add(Delegate action)
-    {
-        if (action is UnityAction<T> unityAction)
-        { 
-            Event += unityAction;
-        }
-    }
-    public void Remove(UnityAction<T> action)
-    {
-        Event -= action;
-    }
-    public void Remove(Delegate action)
-    {
-        if (action is UnityAction<T> unityAction)
+        private UnityAction Event;
+        public void Add(UnityAction action)
         {
-            Event -= unityAction;
+            Event += action;
         }
-    }
-    /// <summary>
-    /// Invoke while passing the value of arg and update latest to reflect this value
-    /// </summary>
-    public void Invoke(T arg)
-    {
-        Latest = arg;
-        Event?.Invoke(arg);
+        public void Add(Delegate action)
+        {
+            if (action is UnityAction unityAction)
+                Event += unityAction;
+        }
+        public void Remove(UnityAction action)
+        {
+            Event -= action;
+        }
+        public void Remove(Delegate action)
+        {
+            if (action is UnityAction unityAction)
+            {
+                Event -= unityAction;
+            }
+        }
+
+        public void Invoke()
+        {
+            Event?.Invoke();
+        }
     }
 
-    /// <summary>
-    /// Invoke using the most recent value passed to the event.
-    /// </summary>
-    public void Invoke()
+    /*
+     * We purposefully only provide an interface with a single 
+     * generic type to encourage clients to define custom data
+     * types whose intent is more clear than that of their 
+     * primative counterparts.
+     * 
+     * Example: Which is immediately more clear?
+     * 
+     * 1. CuddleStarted?.Invoke(true, 8, "Tomorrow"); 
+     * 
+     *         -- or --
+     * 
+     * 2. struct CuddleData
+     *    {
+     *       public bool holdHands;
+     *       public float duration;
+     *       public string subtitle;
+     *    }
+     * 
+     *    CuddleStarted?.Invoke(cuddleData);
+     *    
+     * Clearly, option 2 gives much more detail as to the 
+     * purpose of the parameters. This is exactly the same for 
+     * all number of parameters INCLUDING 1.
+     */
+    public class GameEvent<T> : GameEventBase
     {
-        if (Latest == null)
+        private UnityAction<T> Event;
+        public T Latest;
+
+        public void Add(UnityAction<T> action)
         {
-            throw new Exception("You cannot call Invoke() before Invoke(arg). Did you mean to pass data with this event?");
+            Event += action;
         }
-        Invoke(Latest);
+        public void Add(Delegate action)
+        {
+            if (action is UnityAction<T> unityAction)
+            {
+                Event += unityAction;
+            }
+        }
+        public void Remove(UnityAction<T> action)
+        {
+            Event -= action;
+        }
+        public void Remove(Delegate action)
+        {
+            if (action is UnityAction<T> unityAction)
+            {
+                Event -= unityAction;
+            }
+        }
+        /// <summary>
+        /// Invoke while passing the value of arg and update latest to reflect this value
+        /// </summary>
+        public void Invoke(T arg)
+        {
+            Latest = arg;
+            Event?.Invoke(arg);
+        }
+
+        /// <summary>
+        /// Invoke using the most recent value passed to the event.
+        /// </summary>
+        public void Invoke()
+        {
+            if (Latest == null)
+            {
+                throw new Exception("You cannot call Invoke() before Invoke(arg). Did you mean to pass data with this event?");
+            }
+            Invoke(Latest);
+        }
     }
 }
+
+
--- Assets/Game Events System/GameEvents.cs
+++ Assets/Game Events System/GameEvents.cs
@@ -2,73 +2,11 @@ using UnityEngine;
 
 public class GameEvents : MonoBehaviour
 {
-    #region Setup
     public static GameEvents Instance { get; private set; }
+
     private void Awake()
     {
         if (Instance != null && Instance != this) Destroy(this);
         else Instance = this;
-    }
-    #endregion
-
-    #region Containers
-    public InputContainer Input = new InputContainer();
-    public PlayerContainer Player = new PlayerContainer();
-    public EnemyContainer Enemy = new EnemyContainer();
-    public TimelineContainer Timeline = new TimelineContainer();
-
-    #endregion
-
-
-    public class InputContainer
-    {
-        public GameEvent Jump = new GameEvent();
-        public GameEvent Crouch = new GameEvent();
-        public GameEvent<MoveData> Move = new GameEvent<MoveData>();
-    }
-    public class PlayerContainer
-    {
-        public GameEvent<float> TookDamage = new GameEvent<float>();
-        public GameEvent Died = new GameEvent();
-        public GameEvent Respawned = new GameEvent();
-    }
-    public class EnemyContainer
-    {
-        public GoombaContainer Goomba = new GoombaContainer();
-        public ShitheadContainer Shithead = new ShitheadContainer();
-
-        public class GoombaContainer
-        {
-            public GameEvent Spawned = new GameEvent();
-            public GameEvent Died = new GameEvent();
-        }
-        public class ShitheadContainer
-        {
-            public GameEvent Pooped = new GameEvent();
-            public GameEvent<int> AteShit = new GameEvent<int>();
-        }
-    }
-    public class TimelineContainer
-    {
-        public TitleSequenceContainer Title = new TitleSequenceContainer();
-        public CutSceneContainer CutScene = new CutSceneContainer();
-        public class TitleSequenceContainer
-        {
-
-        }
-        public class CutSceneContainer
-        {
-            public BossDeathContainer BossDeath = new BossDeathContainer();
-            public RomanceContainer Romance = new RomanceContainer();
-            public class BossDeathContainer
-            {
-                
-            }
-            public class RomanceContainer
-            {
-                public GameEvent Kiss = new GameEvent();
-                public GameEvent<CuddleData> Cuddle = new GameEvent<CuddleData>();
-            }
-        }
     }
 }
