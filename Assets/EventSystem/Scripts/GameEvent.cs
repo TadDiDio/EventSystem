7313a3bbafd3a51ffddf8829f2057d2ad38e6880
TadDiDio <taddidio@vt.edu>
2025-03-17 23:09:19 -0400

    Unified API and added + and - overloads

--- Assets/EventSystem/Scripts/GameEvent.cs
+++ Assets/EventSystem/Scripts/GameEvent.cs
@@ -1,81 +1,34 @@
 using System;
-using UnityEngine.Events;
 
 namespace GameEvents
 {
-    /// <summary>
-    /// Interface for game events.
-    /// </summary>
-    public interface IGameEvent
-    {
-        /// <summary>
-        /// Adds a delegate to the event list.
-        /// </summary>
-        /// <param name="action">The delegate to add.</param>
-        public void Subscribe(Delegate action);
+    // Inherit from this to make testing and custom Roslyn analyzer easier
+    public interface IGameEvent { }
 
-        /// <summary>
-        /// Removes a delegate from the event list.
-        /// </summary>
-        /// <param name="action">The delegate to remove.</param>
-        public void Unsubscribe(Delegate action);
-        
-        /// <summary>
-        /// Invokes the event.
-        /// </summary>
-        public void Invoke();
-    }
-    
     /// <summary>
     /// Game event with no parameters.
     /// </summary>
     public class GameEvent : IGameEvent
     {
-        /// <summary>
-        /// The event.
-        /// </summary>
-        public UnityAction Event;
+        private Action _event;
 
         /// <summary>
         /// Adds a delegate to the event list.
         /// </summary>
         /// <param name="action">The delegate to add.</param>
-        public void Subscribe(UnityAction action)
+        public void Subscribe(Action action)
         {
-            Event += action;
-        }
-
-        /// <summary>
-        /// Adds a delegate to the event list.
-        /// </summary>
-        /// <param name="action">The delegate to add.</param>
-        public void Subscribe(Delegate action)
-        {
-            if (action is UnityAction unityAction)
-            {
-                Event += unityAction;
-            }
+            _event += action;
         }
 
         /// <summary>
         /// Removes a delegate from the event list.
         /// </summary>
         /// <param name="action">The delegate to remove.</param>
-        public void Unsubscribe(UnityAction action)
+        public void Unsubscribe(Action action)
         {
-            Event -= action;
-        }
-
-        /// <summary>
-        /// Removes a delegate from event list.
-        /// </summary>
-        /// <param name="action">The delegate to remove.</param>
-        public void Unsubscribe(Delegate action)
-        {
-            if (action is UnityAction unityAction)
-            {
-                Event -= unityAction;
-            }
+            if (_event == null) return;
+            _event -= action;
         }
 
         /// <summary>
@@ -83,7 +36,31 @@ namespace GameEvents
         /// </summary>
         public void Invoke()
         {
-            Event?.Invoke();
+            _event?.Invoke();
+        }
+        
+        /// <summary>
+        /// Overload of the + operator for convenient subscription.
+        /// </summary>
+        /// <param name="gameEvent">'this' GameEvent</param>
+        /// <param name="action">The action to subscribe to it.</param>
+        /// <returns>'this' GameEvent with the subscribed action in its call list.</returns>
+        public static GameEvent operator +(GameEvent gameEvent, Action action)
+        {
+            gameEvent.Subscribe(action);
+            return gameEvent;
+        }
+
+        /// <summary>
+        /// Overload of the - operator for convenient unsubscription.
+        /// </summary>
+        /// <param name="gameEvent">'this' GameEvent</param>
+        /// <param name="action">The action to unsubscribe from it.</param>
+        /// <returns>'this' GameEvent with the unsubscribed action removed from its call list.</returns>
+        public static GameEvent operator -(GameEvent gameEvent, Action action)
+        {
+            gameEvent.Unsubscribe(action);
+            return gameEvent;
         }
     }
 
@@ -91,7 +68,7 @@ namespace GameEvents
      * We purposefully only provide an interface with a single 
      * generic type to encourage clients to define custom data
      * types whose intent is more clear than that of their 
-     * primative counterparts.
+     * primitive counterparts.
      * 
      * Example: Which is immediately more clear for the following event?
      * 
@@ -118,12 +95,9 @@ namespace GameEvents
     /// Game event with one parameter.
     /// </summary>
     /// <typeparam name="T">The type of the parameter to pass in.</typeparam>
-    public class GameEvent<T> : IGameEvent
+    public class GameEvent<T>: IGameEvent
     {
-        /// <summary>
-        /// The event.
-        /// </summary>
-        public UnityAction<T> Event;
+        private Action<T> _event;
         
         /// <summary>
         /// The value of the most recent parameter this event was invoked with.
@@ -134,44 +108,21 @@ namespace GameEvents
         /// Adds a delegate to event list.
         /// </summary>
         /// <param name="action">The delegate to add.</param>
-        public void Subscribe(UnityAction<T> action)
+        public void Subscribe(Action<T> action)
         {
-            Event += action;
-        }
-
-        /// <summary>
-        /// Adds a delegate to the event list.
-        /// </summary>
-        /// <param name="action">The delegate to add.</param>
-        public void Subscribe(Delegate action)
-        {
-            if (action is UnityAction<T> unityAction)
-            {
-                Event += unityAction;
-            }
+            _event += action;
         }
 
         /// <summary>
         /// Removes a delegate from the event list.
         /// </summary>
         /// <param name="action">The delegate to remove.</param>
-        public void Unsubscribe(UnityAction<T> action)
+        public void Unsubscribe(Action<T> action)
         {
-            Event -= action;
+            if (_event == null) return;
+            _event -= action;
         }
 
-        /// <summary>
-        /// Remove a delegate from the event list.
-        /// </summary>
-        /// <param name="action">The delegate to remove.</param>
-        public void Unsubscribe(Delegate action)
-        {
-            if (action is UnityAction<T> unityAction)
-            {
-                Event -= unityAction;
-            }
-        }
-        
         /// <summary>
         /// Invokes the event
         /// </summary>
@@ -179,19 +130,31 @@ namespace GameEvents
         public void Invoke(T arg)
         {
             Latest = arg;
-            Event?.Invoke(arg);
+            _event?.Invoke(arg);
+        }
+        
+        /// <summary>
+        /// Overload of the + operator for convenient subscription.
+        /// </summary>
+        /// <param name="gameEvent">'this' GameEvent</param>
+        /// <param name="action">The action to subscribe to it.</param>
+        /// <returns>'this' GameEvent with the subscribed action in its call list.</returns>
+        public static GameEvent<T> operator +(GameEvent<T> gameEvent, Action<T> action)
+        {
+            gameEvent.Subscribe(action);
+            return gameEvent;
         }
 
         /// <summary>
-        /// Invoke using the most recent value passed to the event.
+        /// Overload of the - operator for convenient unsubscription.
         /// </summary>
-        public void Invoke()
+        /// <param name="gameEvent">'this' GameEvent</param>
+        /// <param name="action">The action to unsubscribe from it.</param>
+        /// <returns>'this' GameEvent with the unsubscribed action removed from its call list.</returns>
+        public static GameEvent<T> operator -(GameEvent<T> gameEvent, Action<T> action)
         {
-            if (Latest == null)
-            {
-                throw new Exception("You cannot call Invoke() before Invoke(arg). Did you mean to pass data with this event?");
-            }
-            Invoke(Latest);
+            gameEvent.Unsubscribe(action);
+            return gameEvent;
         }
     }
 }
